34eac37c1b9f0cc492e0ffa6dfe39dd40a6d8a0c
RandallZPorter <42816381+RandallZPorter@users.noreply.github.com>
2019-01-20 00:03:56 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
+<p style="line-height: 18px; font-size: 18px;  font-family: times;">
+Click "<i>Load samples</i>" to view and edit more JS samples.<br>
+<br>
+Labyrinth generated with JavaScript:<br><br>
+
+<button onclick="myFunction()">Click me</button>
+<script>
+
 function myFunction() {
   document.write("Hello, World!");
 }
+
+</script>
+</p>
